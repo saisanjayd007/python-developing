cbe3f4a5b0139b7b13adf05c205d5441d1651763
saisanjayd007 <eatingwithsanju@gmail.com>
2025-08-19 10:23:19 +0530

    Update calculator python project.py

--- calculator python project.py
+++ calculator python project.py
@@ -1,21 +1,17 @@
 def calculator():
     print("Simple Calculator cerated by Sanjay")
 
-    # Prompt the user to enter two numbers
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
 
-    # Display operation choices
     print("Select operation:")
     print("1. Addition (+)")
     print("2. Subtraction (-)")
     print("3. Multiplication (*)")
     print("4. Division (/)")
 
-    # Prompt the user to select an operation
-    choice = input("Enter your choice (1/2/3/4): ")
-
-    # Perform the operation based on user choice
+    choice = input("Enter your choice (1/2/3/4): "
+                   
     if choice == '1':
         result = num1 + num2
         operation = '+'
@@ -41,3 +37,4 @@ def calculator():
 
 # Run the calculator function
 calculator()
+
